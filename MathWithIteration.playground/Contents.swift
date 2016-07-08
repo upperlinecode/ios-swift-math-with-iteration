65a9b17a96d41f469d77ced9d28d433032aa21b5
carsonlevine <carsonglevine@me.com>
2016-07-08 15:33:29 -0400

    first commit

---
+++ MathWithIteration.playground/Contents.swift
@@ -0,0 +1,58 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Here are a few math problems that are very difficult without iteration, but much easier once you add a nice for-in loop.
+//Give these problems a shot. Try to make your code as simple as possible. If you are typing 100 lines for one of these problems,
+//there is probably an easier way to do it.
+
+
+
+
+
+//1. Big Sums
+//The sum of the digits from 1 to 10 (1 + 2 + ... + 10) is 55
+//Find the sum of the digits from 1 to 487
+
+
+
+
+
+//2. Age in Seconds
+//There are 51 students taking Intro to Swift. The age in years of each student is listed in the array 'ages'. Create a new array, agesInDays. It should contain their ages, but with the age in days instead of years. You can disregard leap years and assume every year is 365 days.
+let ages = [15, 16, 16, 14, 17, 17, 14, 15, 15, 16, 17, 15, 17, 18, 15, 16, 17, 14, 15, 15, 16, 17, 16, 15, 16, 14, 18, 14, 18, 17, 15, 16, 14, 16, 16, 15, 17, 15, 15, 16, 16, 17, 16, 15, 15, 16, 15, 16, 14, 17, 17]
+
+
+
+
+
+
+//3. Sum of Multiples
+// The multiples of 3 and 5 that are less than or equal to 10 are: 3, 5, 6, 9, and 10. The sum of these multiples is 33.
+//Find the sum of all of the multiples of 3 and 5 that are less than or equal to 1000000
+
+
+
+
+
+//4. Difference of Squares
+//The sum of the squares from 1 to 5 is (1^2 + 2^2 + 3^2 + 4^2 + 5^2) = 55
+//The square of the sum from 1 to 5 is (1 + 2 + 3 + 4 + 5)^2 = 225
+//The difference between the square of the sum from 1 to 5 and the sum of the squares from 1 to 5 is 225 - 55 = 170
+//Find the difference between the square of the sum from 1 to 1000 and the sum of the squares from 1 to 1000
+//Notice that the computer starts requiring a noticable amount of time to make this calculation. How large does the sequence have to
+//become before it takes several seconds to iterate? (There is no right answer to this second question, just something to think about)
+
+
+
+
+
+
+
+
+
+//Answers:
+//1. 118828
+//2. [5475, 5840, 5840, 5110, 6205, 6205, 5110, 5475, 5475, 5840, 6205, 5475, 6205, 6570, 5475, 5840, 6205, 5110, 5475, 5475, 5840, 6205, 5840, 5475, 5840, 5110, 6570, 5110, 6570, 6205, 5475, 5840, 5110, 5840, 5840, 5475, 6205, 5475, 5475, 5840, 5840, 6205, 5840, 5475, 5475, 5840, 5475, 5840, 5110, 6205, 6205]
+//3. 233334166668
+//4. 333333000
